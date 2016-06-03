aa4ad945205760b13f1e5e54263fa9f5c0df0768
b1f6c1c4 <tujinzheng@126.com>
2016-06-03 19:00:38 +0800

    add main module and assign pin

---
+++ Calc.v
@@ -0,0 +1,13 @@
+`default_nettype none
+module Calc(
+	input CLK,
+	input RST,
+	input [3:0] H,
+	output [3:0] V,
+	output [0:3] SD,
+	output [0:7] SEG,
+	output [7:0] LD,
+	output Buzz
+   );
+
+endmodule
