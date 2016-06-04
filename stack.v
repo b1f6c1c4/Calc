a734a038df066c3fabca0574d1c5a457614f7c66
b1f6c1c4 <tujinzheng@126.com>
2016-06-04 18:54:22 +0800

    fix stack

--- stack.v
+++ stack.v
@@ -4,6 +4,7 @@ module stack(
    input Clock,
    input Reset,
    inout [N-1:0] data,
+   output is_empty,
    input [`SC_N-1:0] cmd
    );
    parameter N = 16;
@@ -14,6 +15,7 @@ module stack(
    reg [N-1:0] buff;
    reg out_ena;
    assign data = out_ena ? buff : {N{1'bz}};
+   assign is_empty = ~|ptr;
    
    always @(posedge Clock, negedge Reset)
       if (~Reset)
@@ -31,16 +33,18 @@ module stack(
                   out_ena <= 1'b0;
                end
             `SC_POP:
-               begin
-                  buff <= ram[ptr];
-                  ptr <= ptr - 6'b1;
-                  out_ena <= 1'b1;
-               end
+               if (|ptr)
+                  begin
+                     buff <= ram[ptr - 6'b1];
+                     ptr <= ptr - 6'b1;
+                     out_ena <= 1'b1;
+                  end
             `SC_TOP:
-               begin
-                  buff <= ram[ptr];
-                  out_ena <= 1'b1;
-               end
+               if (|ptr)
+                  begin
+                     buff <= ram[ptr - 6'b1];
+                     out_ena <= 1'b1;
+                  end
             default:
                out_ena <= 1'b0;
          endcase
