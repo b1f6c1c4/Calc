a28fb241596ae600bd0050ecdee83759e5fc9eeb
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-06-27 10:25:10 +0800

    new alu

--- alu.v
+++ alu.v
@@ -1,21 +1,26 @@
 `default_nettype none
 module alu(
-   input signed [N-1:0] A,
-   input signed [N-1:0] B,
-   input [AC_N-1:0] cmd,
-   output reg signed [N-1:0] C
+   input signed [N-1:0] data_a,
+   input signed [N-1:0] data_b,
+   input carry_in,
+   input [AC_N-1:0] CS,
+   output reg signed [N-1:0] S,
+   output reg zero,
+   output reg carry_out
    );
-   parameter N = 16;
+   parameter N = 8;
 `include "ALU_INTERFACE.v"
+   
+   assign zero = ~|S;
    
    always @(*)
       case (cmd)
-         AC_AD: C <= A + B;
-         AC_SB: C <= A - B;
-         AC_MU: C <= A * B;
-         AC_DI: C <= A / B;
-         AC_RM: C <= A % B;
-         default: C <= {N{1'bx}};
+         AC_AD: {carry_out,S} <= data_a + data_b + carry_in;
+         AC_SB: {carry_out,S} <= data_a - data_b + carry_in;
+         AC_AN: begin S <= data_a & data_b; carry_out <= 1'b0; end
+         AC_OR: begin S <= data_a | data_b; carry_out <= 1'b0; end
+         AC_LS: begin S <= data_a < data_b; carry_out <= 1'b0; end
+         default: begin S <= {N{1'bx}}; carry_out <= 1'b0; end
       endcase
    
 endmodule
